4e442e16637b41d8d52e60841dfc26ea64f95f08
Yijie Tong <yijie.tong@pwc.com>
2021-09-01 11:17:19 -0400

    Demonstrate keyboard api issue

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { Keyboard } from '@capacitor/keyboard';
+import { StatusBar, Style } from '@capacitor/status-bar';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
   constructor() {}
 
+  ngOnInit() {
+    StatusBar.setStyle({style: Style.Dark});
+    // keyboard will resize webview when the following line is commented
+    StatusBar.setOverlaysWebView({overlay: true});
+  }
 }
